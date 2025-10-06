0fdc4091c72bad052e1b5390889ac1b330c1e1cc
Igor-Pupk0 <igor_pupko@proton.me>
2025-10-06 21:48:24 +0700

    V 0.10.1
    Что нового:
    - Починил авторизацию

--- src/bot/core/states.py
+++ src/bot/core/states.py
@@ -2,11 +2,16 @@ from .user import User
 from .storage import users_states, user_auths
 
 def get_user_status(telegram_id):
-    user_states = users_states.get(telegram_id)
+    user_states: User = users_states.get(telegram_id)
     if user_states is not None:
+        print(1)
         if user_states.API == None and user_auths.get(telegram_id) != None:
+            print(2)
             user_list = user_auths.get(telegram_id)
-            user_states = User(user_list['User_obj'])
+            print(user_states.auth_status)
+            user_obj = user_list.get('User_obj')
+            if user_obj != None:
+                user_states = User(user_obj)
         return user_states
     user_list = user_auths.get(telegram_id)
     if user_list == None:
