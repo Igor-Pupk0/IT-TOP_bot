23ad81e4a1ba0ba3a524d2be010ace199f9eb0d1
Igor-Pupk0 <igor_pupko@proton.me>
2025-10-06 21:57:19 +0700

    V 0.10.1.1
    Что нового:
    - Починил broadcast

--- src/bot/core/states.py
+++ src/bot/core/states.py
@@ -11,7 +11,7 @@ def get_user_status(telegram_id):
             print(user_states.auth_status)
             user_obj = user_list.get('User_obj')
             if user_obj != None:
-                user_states = User(user_obj)
+                user_states.API = user_obj
         return user_states
     user_list = user_auths.get(telegram_id)
     if user_list == None:
