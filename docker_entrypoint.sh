db4f6e69c08aa2284e91194442d6b672280921c0
Igor-Pupk0 <igor_pupko@proton.me>
2025-12-03 20:15:06 +0700

    V 0.22.6.2

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -4,9 +4,6 @@ export $(cat .env | xargs)
 TEMPLATE="./files/nginx.conf.example"
 OUTPUT="./files/nginx.conf"
 
-export WEBHOOK_DOMAIN="${WEBHOOK_DOMAIN:-example.com}"
-export WEBHOOK_ENDPOINT="${WEBHOOK_ENDPOINT:-webhook}"
-
 rm -f "$OUTPUT"
 
 cp "$TEMPLATE" "$OUTPUT"
