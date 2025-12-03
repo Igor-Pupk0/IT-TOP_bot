c550a47ef0acf64a91cdcca418531a9bc82934c9
Igor-Pupk0 <igor_pupko@proton.me>
2025-12-03 20:00:49 +0700

    V 0.22.6.1
    Попытка исправить скрипт

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 export $(cat .env | xargs)
-# Указываем файл для обработки
-FILE="./files/nginx.conf.example"
 
-# Или использовать envsubst, если он установлен
-envsubst < "$FILE" > ./files/nginx.conf
+TEMPLATE="./files/nginx.conf.example"
+OUTPUT="./files/nginx.conf"
+
+export WEBHOOK_DOMAIN="${WEBHOOK_DOMAIN:-example.com}"
+export WEBHOOK_ENDPOINT="${WEBHOOK_ENDPOINT:-webhook}"
+
+rm -f "$OUTPUT"
+
+cp "$TEMPLATE" "$OUTPUT"
+
+
+for var in $(compgen -e); do
+    value=$(printenv "$var")
+
+    safe_value=$(printf '%s\n' "$value" | sed 's/[[\.*^$/]/\\&/g')
+
+    sed -i "s|\${$var}|$safe_value|g" "$OUTPUT"
+done
